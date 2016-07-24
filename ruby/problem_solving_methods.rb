57847cce724d353358b594e48e114e887b0eab20
Tatiana Sutherland <tsutherl@oberlin.edu>
2016-07-24 15:49:10 -0500

    Writes method that calculates fibonacci numbers

--- ruby/problem_solving_methods.rb
+++ ruby/problem_solving_methods.rb
@@ -17,3 +17,32 @@ end
 arr = [42, 89, 23, 1, 1]
 p find_num(arr, 1)
 
+#have array [0,1]
+#while index is grater than than 1 and less than length asked
+  #add index 0 and 1 together and store in var new_index
+  #add sum to array
+  #new_index = 0 
+  #add 1 to index
+#return array
+
+def fib(num_fib_terms)
+  fib_ary = [0,1]
+  index = 1
+  new_index = 0
+  while fib_ary.length < num_fib_terms
+    new_index = fib_ary[index] + fib_ary[index - 1]
+    fib_ary << new_index
+    new_index = 0
+    index += 1
+  end
+  fib_ary
+end
+
+p fib(6)
+test = fib(100)
+if test[99] == 218922995834555169026
+  puts "Works!"
+end
+
+
+
